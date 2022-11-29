2fffcefdb95af96e6f1682759b7f90e23d527505
Szyrwel <potato_father@list.ru>
2022-11-29 14:13:58 +0300

    feat: add solution getRectangleString in strings-tasks (strings-tasks done)

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -202,8 +202,18 @@ function extractEmails(str) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleString(width, height) {
+  let rectangle = '';
+  for (let i = 0; i < height; i += 1) {
+    if (i === 0) {
+      rectangle += `┌${'─'.repeat(width - 2)}┐\n`;
+    } else if (i === height - 1) {
+      rectangle += `└${'─'.repeat(width - 2)}┘\n`;
+    } else {
+      rectangle += `│${' '.repeat(width - 2)}│\n`;
+    }
+  }
+  return rectangle;
 }
 
 
