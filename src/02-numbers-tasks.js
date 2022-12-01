c51e81f38feaeb43c77c97e67273992ed3a69d68
Szyrwel <potato_father@list.ru>
2022-12-01 22:32:39 +0300

    feat: add solution isPrime in numbers-tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -203,9 +203,18 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n / 2 + 1; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /**
  * Tries to convert value to number and returns it if conversion was successful;
