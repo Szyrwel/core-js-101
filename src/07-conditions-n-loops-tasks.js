e429e128c4876d52a306861bdb89985653c527c6
Szyrwel <potato_father@list.ru>
2023-01-02 19:55:51 +0300

    feat: add solution getIntervalString  in conditions-n-loops-tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -206,8 +206,11 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const min = a > b ? b : a;
+  const max = a > b ? a : b;
+  const str = isStartIncluded ? `[${min}, ${max}` : `(${min}, ${max}`;
+  return isEndIncluded ? `${str}]` : `${str})`;
 }
 
 
